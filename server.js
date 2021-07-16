6ffa7dc4768a4512b3e75def1a50907bc21c8c29
JasperZhou <jasperjzhou@gmail.com>
2021-07-17 04:13:03 +0800

    set Template

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 var app = express();
 
 function main() {
-    app.use(express.static(__dirname+'/public'))
+    app.use(express.static(__dirname+'/public'));
     app.listen(process.env.PORT || 3000);
 }
 main();
